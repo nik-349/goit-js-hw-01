d09e4e3870544c8b1415cb16e6e6ff7fcac029c3
nik-349 <nik349@icloud.com>
2020-12-06 01:38:17 +0200

    new-rep

--- task-2.js
+++
@@ -1,9 +0,0 @@
-const total = 100;
-const ordered = 1020;
-
-
-if (ordered > total) {
-    console.log('На складе недостаточно тoваров!');
-} else { ordered <= total
-    console.log('Заказ оформлен, с вами свяжется менеджер');
-}
